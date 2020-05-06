289ee2d921f92a217906b49b4a24779579b710b3
sayan <sayansahoo78@gmail.com>
2020-05-06 21:26:55 +0530

    project initiated

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const axios = require('axios');
+const { v4: uuidv4 } = require("uuid");
+const token = uuidv4();
+app.use(cors());
+const GOOGLE_API_KEY = 'AIzaSyBWTjvqY0SEpqERzczBI_7WHFuStg3qRQk';
+app.get("/", async (req, res) => {
+    console.log(req, 'req')
+    let {data} = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.query.place}&key=${GOOGLE_API_KEY}&sessiontoken=${token}`
+    );
+     console.log(data, 'data')
+    res.json(data);
+  });
+
+app.listen(process.env.PORT || 5000);
