dcaa9e2e75f1ad9bf38084713a75bae0aca8b1f3
sayan <sayansahoo78@gmail.com>
2020-05-07 13:26:43 +0530

    added key

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const { v4: uuidv4 } = require("uuid");
 const token = uuidv4();
 app.use(cors());
-const GOOGLE_API_KEY = '';
+const GOOGLE_API_KEY = 'AIzaSyBM3_IkHUdhs1MiUOaF4jGMr6AdReLV534';
 app.get("/", async (req, res) => {
     let {data} = await axios.get(
       `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${req.query.place}&key=${GOOGLE_API_KEY}&sessiontoken=${token}`
